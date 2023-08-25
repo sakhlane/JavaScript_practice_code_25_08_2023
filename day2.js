56f4b3b456bc9aede0d8e0c77449f13c0eb1c72f
sakhlane <107583915+sakhlane@users.noreply.github.com>
2023-08-25 16:06:16 -0400

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,38 @@
+console.log('we are in day 2');
+
+/** Data type 
+ *  ===> data type is a type of data or information which is to be going to store in a variable 
+ * 1. string
+ * 2. number
+ * 3. bool
+ * 4. array
+ * 5. object */
+
+// string ==> string written in single coute 
+
+let str = 'sakhlane';
+console.log(str);
+
+// number
+let phoneNumber = 8892935368;
+console.log(phoneNumber);
+
+//bool there are only two values 1. true 2.false
+let isMonday = false;
+console.log(isMonday);
+
+// arrays ==> related values
+let carBrand = ['audi', 'bmw', 'omni', 'safari']
+console.log(carBrand);
+
+// object ==> retated item for a particular object or thing (related values to a particular item)
+let student = {
+    name : 'sakhlane',
+    batch : 'FSR_230523',
+    univercity :'EDYODA Digital university' ,
+}
+console.log(student);
+
+
+
+
