02e20b444891028a723a612a589b1624da8933c4
sakhlane <107583915+sakhlane@users.noreply.github.com>
2023-08-29 14:01:02 -0400

    Add files via upload

---
+++ day2_oarators.js
@@ -0,0 +1,50 @@
+console.log('oparators')
+
+// arithmetic oparators
+ /**
+       + ==> plus
+      - ==> minus
+       * ==> multiplication
+       / ==> division
+       % ==> modulus or reminder
+  */
+
+    let a =20;
+    let b = 30; 
+    console.log(a+b);// addition
+    console.log(a - b) // subtraction
+    console.log( a * b) // multiplication
+    console.log(a / b)// division
+    console.log( a % b) // modulus or reminder
+
+    // assaignment oparator 
+     // ' = ' sign is used for assaignment 
+
+//  Comparison Operators: 
+  /* < ===> greater than , 
+     > ===> less than ,
+     <= ----> greater than or equal to
+     >=  ----> less than or equal to , 
+     ==   ----> equal to , 
+     ===  ---> value qual to and as well as data type ,
+     !=   ----> not equal to 
+*/
+  console.log(b < a); // return  ture or false based on condition
+  console.log(b > a); // return  ture or false based on condition
+  console.log(b >= a); // return  ture or false based on condition
+  console.log(b <= a); // return  ture or false based on condition
+  console.log(b == a); // return  ture or false based on condition
+  console.log(b === a); // return  ture or false based on condition and it cheks type also 
+  console.log(a != b) ; //return  ture or false based on condition
+// Conditional Operator: 
+// Ternary Operator : ?=
+ (a > b) ? console.log('hi') : console.log('buy');
+// Logical Operators : &&, ||
+ (1 && 0) ? console.log('true') : console.log('false') ;
+ (true && true) ? console.log('true') : console.log('false');
+ (true || false) ? console.log('true') : console.log('false');
+ (true || 0) ? console.log('true') : console.log('false');
+ 
+// Type Operators: typeof and instanceof;
+console.log(typeof(a));
+
